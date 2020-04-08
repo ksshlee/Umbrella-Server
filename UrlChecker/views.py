96ad8f2b56f9ba8ea6442d51d29ab11ccd4b9807
ksshlee <ksshlee@naver.com>
2020-04-08 20:36:07 +0900

    응답값 변경

--- UrlChecker/views.py
+++ UrlChecker/views.py
@@ -4,7 +4,7 @@ from .models import Urldata
 from .forms import UrlForm
 from .key_features.url_look_up import url_look_up
 from .key_features.valid_url import valid
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -18,7 +18,15 @@ def valid_url(request):
         url = form.data['url'] # form 에서 url 추출
         results = url_look_up(url) # url whois 로 조회 결과 results에 담기
 
+    if results is False:
+        # url 형식이 아니라서 url 형식 맞는지 알려줘야함
+        # 아니면 url 의 whois 조회가 안되는거임
+        # 404
+        return JsonResponse({'status':'404'},status=404)
+    else :
         if valid(results):
-            return HttpResponse('<h1>통과</h1>')
+            # 200
+            return JsonResponse({'status':'200'},status=200)
         else :
-            return HttpResponse('<h1>실패</h1')
+            # 403 에러
+            return JsonResponse({'status':'403'},status=403)
